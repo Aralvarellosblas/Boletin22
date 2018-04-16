8162072109377342bbd1ce0930e18517720a7010
arturo <arturo@ArturoMSI>
2018-04-16 08:51:42 +0200

    Ejercicio Finalizado

---
+++ Ejercicio22_2/src/ejercicio22_2/Ejercicio22_2.java
@@ -0,0 +1,16 @@
+package ejercicio22_2;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio22_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario f=new Formulario();
+    }
+
+}
